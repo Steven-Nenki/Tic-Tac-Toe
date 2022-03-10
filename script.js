2298249e0a9bcc0b23b848d618aec5a1bb69867c
Steven <cldsteve03@gmail.com>
2022-03-10 22:36:33 +0100

    Add backgound and 'Belle remonté'

--- script.js
+++ script.js
@@ -75,12 +75,19 @@ const jouerCase = (e) => {
     if(state.joueurEnCours == 1) {
       state.scoreJ1++
       score1.textContent = state.scoreJ1;
-      if(state.scoreJ1 == 3) {
+      if(state.scoreJ1 == 3 && state.scoreJ2 < 3) {
         alert(`Victoire écrasante du joueur ${state.joueurEnCours}`)
+      } else if (state.scoreJ1 == 3 && state.scoreJ2 >= 3) {
+        alert(`Belle remonter de la part du joueur ${state.joueurEnCours} !`)
       }
     } else {
       state.scoreJ2++
       score2.textContent = state.scoreJ2;
+      if(state.scoreJ2 == 3 && state.scoreJ1 < 3) {
+        alert(`Victoire écrasante du joueur ${state.joueurEnCours}`)
+      } else if (state.scoreJ2 == 3 && state.scoreJ1 >= 3) {
+        alert(`Belle remonter de la part du joueur ${state.joueurEnCours} !`)
+      }
     }
     resetState();
     cases.forEach(c => (c.textContent = ''));
